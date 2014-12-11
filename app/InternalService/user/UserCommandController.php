735155263e2e415d4e15bfd473451bd6b51a9623
Kevin Washington <kevw12188@gmail.com>
2014-12-11 16:30:56 -0500

    added some guides for next session.

--- app/InternalService/user/UserCommandController.php
+++ app/InternalService/user/UserCommandController.php
@@ -16,14 +16,36 @@ class UserCommandController {
 
     }
 
-    public function store()
+    public function store($credentials = array())
     {
+        //check [conditional]
 
+            //validate that array contains correct keys
+
+            //validate that array contains correct length
+
+            //validate that no other users are using any unique values
+
+            //validate format of all values
+
+                // - if yes
+
+                    //create new user
+
+                    //add email and password to user
+
+                    //save user in database
+
+                    //return saved in string
+
+                // - if no
+
+                    //return Credentials invalid.
     }
 
     public function show()
     {
-
+        //
     }
 
     public function update()
